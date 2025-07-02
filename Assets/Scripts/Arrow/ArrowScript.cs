8264e1ee6b547c2cfea352e2010c94c94022e366
DanielAlmuster <danialmuster@gmail.com>
2025-07-02 18:36:34 +0200

    Creacion de flecha, prefabs y rotación en la animación del personaje principal

---
+++ Assets/Scripts/Arrow/ArrowScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D Rigidbody2D;
+    private Vector2 Direction;
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Rigidbody2D.velocity = transform.right * Speed;
+
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        Direction = direction;
+    }
+
+}
--- Assets/Scripts/Character/Seeker_Movement.cs
+++ Assets/Scripts/Character/Seeker_Movement.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Seeker_Movement : MonoBehaviour
 {
+    public GameObject ArrowPrefab;
     public float Speed;
     public float JumpForce;
     private bool Grounded;
     private Rigidbody2D RigidBody2D;
+    private Animator Animator;
     private float Horizontal;
-    
+
     void Start()
     {
         RigidBody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+
+        RigidBody2D.freezeRotation = true;
     }
 
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        Animator.SetBool("Running", Horizontal != 0.0f);
 
         Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
         if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
@@ -26,9 +36,13 @@ public class Seeker_Movement : MonoBehaviour
         }
         else Grounded = false;
 
-        if (Input.GetKeyDown(KeyCode.W)&&Grounded==true)
+        if (Input.GetKeyDown(KeyCode.W) && Grounded == true)
         {
             Jump();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
         }
     }
 
@@ -40,5 +54,16 @@ public class Seeker_Movement : MonoBehaviour
     private void Jump()
     {
         RigidBody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    private void Shoot()
+    {
+        //Detector de dirección
+        Vector3 direction;
+        if (transform.localScale.x == 1.0f) direction = Vector2.right;
+        else direction = Vector2.left;
+        //Utiliza el prefab indicado y lo duplica en una parte del mundo, sin rotación
+        GameObject arrow = Instantiate(ArrowPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        arrow.GetComponent<ArrowScript>().SetDirection(direction);
     }
 }
