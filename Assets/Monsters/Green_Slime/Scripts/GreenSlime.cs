490a51805b82b82d1bdb5b5a83794708dbc1dc2c
DanielAlmuster <danialmuster@gmail.com>
2025-07-17 15:44:06 +0200

    Funcionamiento Slime Verde

--- Assets/Monsters/Green_Slime/Scripts/GreenSlime.cs
+++ Assets/Monsters/Green_Slime/Scripts/GreenSlime.cs
@@ -5,12 +5,38 @@ using UnityEngine;
 public class GreenSlime : MonoBehaviour
 {
     public GameObject Seeker;
+    private Rigidbody2D rb;
+    private float jumpTimer = 2f;
+    public float jumpForce;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
 
     private void Update()
     {
         //Se resta la posicion de este objeto con la del seleccionado para que vea para ese objeto
         Vector3 direction = Seeker.transform.position - transform.position;
-        if (direction < 0.0f) transform.localScale = new Vector3(-0.5f, 0.5f, 1.0f);
-        else if (direction > 0.0f) transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+        if (direction.x < 0.0f) transform.localScale = new Vector3(-0.5f, 0.5f, 1.0f);
+        else if (direction.x > 0.0f) transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+
+        jumpTimer -= Time.deltaTime;
+
+        if (jumpTimer <= 0f)
+        {
+            JumpToSeeker();
+            jumpTimer = 2f;
+        }
     }
+
+    private void JumpToSeeker()
+{
+    float directionX = Mathf.Sign(Seeker.transform.position.x - transform.position.x);
+
+    Vector2 jumpDirection = new Vector2(directionX, 1f).normalized;
+
+    rb.AddForce(jumpDirection * jumpForce, ForceMode2D.Impulse);
+}
 }
