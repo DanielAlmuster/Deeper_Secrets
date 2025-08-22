21eb2d9da93a3ee55b799df595e04b995e98466b
DanielAlmuster <danialmuster@gmail.com>
2025-08-22 18:47:09 +0200

    Script para paredes romplibles

--- Assets/Scripts/Enviroment/Button.cs
+++ Assets/Scripts/Enviroment/Button.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    
+    private Rigidbody2D rigidbody2D;
+    public GameObject wall;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Seeker"))
+        {
+            Destroy(wall);
+            SpriteRenderer sr = rigidbody2D.GetComponent<SpriteRenderer>();
+            sr.color = Color.black;
+        }
+    }
     
 }
